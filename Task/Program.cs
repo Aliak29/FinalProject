d4e3529b3d7ebe2c8bbeed6982c48a13f2e1dc36
Aliya <karimova_aly@mail.ru>
2022-12-25 22:37:13 +0500

    Adding program

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,7 +1,34 @@
-﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+﻿//Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
 //Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 //При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 //Примеры:
 //[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 //[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 //[“Russia”, “Denmark”, “Kazan”] → []
+
+Console.WriteLine("Введите несколько чисел/букв/символов через пробел");
+string array1 = Console.ReadLine();
+string[] array1Split = array1.Split();
+string[] array2 = new string[array1Split.Length];
+void SortArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SortArray(array1Split, array2);
+PrintArray(array2);
